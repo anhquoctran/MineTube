4986126c0bba274dede7a1a20b7f9091bed7ce2f
Anh Quoc Tran <quoc.trananh@students.hueic.edu.vn>
2016-07-04 10:05:47 +0700

    Add folder

--- YoutubeApisDemo/frmHome.Designer.cs
+++ YoutubeApisDemo/frmHome.Designer.cs
@@ -35,21 +35,24 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.pnlMain = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
             this.lblSoftversion = new System.Windows.Forms.Label();
             this.btnVideoInfo = new System.Windows.Forms.Button();
             this.btnPlaylist = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.pnlMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label1.ForeColor = System.Drawing.Color.Gray;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(735, 139);
+            this.label1.Size = new System.Drawing.Size(814, 139);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to\r\nYouTube API Demo Project";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -57,10 +60,11 @@
             // 
             // btnClose
             // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnClose.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.ForeColor = System.Drawing.Color.DarkGreen;
-            this.btnClose.Location = new System.Drawing.Point(667, 424);
+            this.btnClose.Location = new System.Drawing.Point(746, 557);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(79, 28);
             this.btnClose.TabIndex = 3;
@@ -74,7 +78,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label2.Location = new System.Drawing.Point(176, 341);
+            this.label2.Location = new System.Drawing.Point(151, 419);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(82, 17);
             this.label2.TabIndex = 4;
@@ -87,7 +91,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label3.Location = new System.Drawing.Point(508, 341);
+            this.label3.Location = new System.Drawing.Point(387, 419);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(73, 17);
             this.label3.TabIndex = 5;
@@ -98,6 +102,7 @@
             // 
             this.pnlMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pnlMain.Controls.Add(this.label4);
+            this.pnlMain.Controls.Add(this.btnSearch);
             this.pnlMain.Controls.Add(this.btnClose);
             this.pnlMain.Controls.Add(this.lblSoftversion);
             this.pnlMain.Controls.Add(this.label3);
@@ -107,27 +112,17 @@
             this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlMain.Location = new System.Drawing.Point(0, 0);
             this.pnlMain.Name = "pnlMain";
-            this.pnlMain.Size = new System.Drawing.Size(759, 462);
+            this.pnlMain.Size = new System.Drawing.Size(838, 595);
             this.pnlMain.TabIndex = 6;
             this.pnlMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlMain_MouseDown);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label4.ForeColor = System.Drawing.Color.Gray;
-            this.label4.Location = new System.Drawing.Point(351, 235);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 40);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "OR";
-            // 
             // lblSoftversion
             // 
+            this.lblSoftversion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.lblSoftversion.AutoSize = true;
             this.lblSoftversion.BackColor = System.Drawing.Color.Transparent;
             this.lblSoftversion.ForeColor = System.Drawing.Color.Gray;
-            this.lblSoftversion.Location = new System.Drawing.Point(11, 437);
+            this.lblSoftversion.Location = new System.Drawing.Point(11, 570);
             this.lblSoftversion.Name = "lblSoftversion";
             this.lblSoftversion.Size = new System.Drawing.Size(65, 15);
             this.lblSoftversion.TabIndex = 6;
@@ -136,12 +131,12 @@
             // btnVideoInfo
             // 
             this.btnVideoInfo.FlatAppearance.BorderSize = 0;
-            this.btnVideoInfo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnVideoInfo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Azure;
             this.btnVideoInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVideoInfo.Image = ((System.Drawing.Image)(resources.GetObject("btnVideoInfo.Image")));
-            this.btnVideoInfo.Location = new System.Drawing.Point(469, 186);
+            this.btnVideoInfo.Location = new System.Drawing.Point(344, 244);
             this.btnVideoInfo.Name = "btnVideoInfo";
-            this.btnVideoInfo.Size = new System.Drawing.Size(150, 152);
+            this.btnVideoInfo.Size = new System.Drawing.Size(167, 172);
             this.btnVideoInfo.TabIndex = 2;
             this.toolTip1.SetToolTip(this.btnVideoInfo, "Get Video Information");
             this.btnVideoInfo.UseVisualStyleBackColor = true;
@@ -150,23 +145,49 @@
             // btnPlaylist
             // 
             this.btnPlaylist.FlatAppearance.BorderSize = 0;
-            this.btnPlaylist.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnPlaylist.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Azure;
             this.btnPlaylist.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPlaylist.Image = ((System.Drawing.Image)(resources.GetObject("btnPlaylist.Image")));
-            this.btnPlaylist.Location = new System.Drawing.Point(145, 186);
+            this.btnPlaylist.Location = new System.Drawing.Point(106, 244);
             this.btnPlaylist.Name = "btnPlaylist";
-            this.btnPlaylist.Size = new System.Drawing.Size(150, 152);
+            this.btnPlaylist.Size = new System.Drawing.Size(169, 172);
             this.btnPlaylist.TabIndex = 1;
             this.toolTip1.SetToolTip(this.btnPlaylist, "Get Playlist Information");
             this.btnPlaylist.UseVisualStyleBackColor = true;
             this.btnPlaylist.Click += new System.EventHandler(this.btnChannel_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Azure;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Image = ((System.Drawing.Image)(resources.GetObject("btnSearch.Image")));
+            this.btnSearch.Location = new System.Drawing.Point(569, 244);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(167, 172);
+            this.btnSearch.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.btnSearch, "Get Video Information");
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(604, 419);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Search Videos";
+            this.toolTip1.SetToolTip(this.label4, "Get Video Information");
             // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(759, 462);
+            this.ClientSize = new System.Drawing.Size(838, 595);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pnlMain);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
@@ -194,5 +215,6 @@
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.Label lblSoftversion;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSearch;
     }
 }
